1aa6ba69bbf3b06dae645639440f053c96088ae8
engine95 <mustangsroc@gmail.com>
2018-08-22 07:17:43 -0600

    build scripts and missed EGL clocks

---
+++ 810-build.sh
@@ -0,0 +1,34 @@
+#Build 810
+
+#!/bin/bash
+DTS=arch/arm/boot/dts
+RDIR=$(pwd)
+# Toolchain
+export ARCH=arm
+export CROSS_COMPILE=../toolchains/UBERTC-4.9/bin/arm-eabi-
+make clean && make mrproper
+rm -rf dt.img
+make gts210wifi_02_defconfig
+make exynos5433-gts210_eur_open_03.dtb
+make exynos5433-gts210wifi_eur_open_04.dtb
+make exynos5433-gts210wifi_eur_open_05.dtb
+make exynos5433-gts210wifi_eur_open_06.dtb
+make ARCH=arm -j6
+echo -n "Build dt.img......................................."
+
+./tools/dtbtool -o ./dt.img -v -s 2048 -p ./scripts/dtc/ $DTS/
+# get rid of the temps in dts directory
+rm -rf $DTS/.*.tmp
+rm -rf $DTS/.*.cmd
+rm -rf $DTS/*.dtb
+
+# Calculate DTS size for all images and display on terminal output
+du -k "./dt.img" | cut -f1 >sizT
+sizT=$(head -n 1 sizT)
+echo "Combined DT Size = $sizT Kb"
+rm -rf sizT
+
+cp /home/matt/android/rb1-kernel/arch/arm/boot/zImage /home/matt/Kernels
+
+cp /home/matt/android/rb1-kernel/dt.img /home/matt/Kernels
+
